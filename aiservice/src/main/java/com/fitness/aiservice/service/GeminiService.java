1146723bdb42292ff28e13bcfcf43d181fd4dfe0
HUJAIFA ANSARI <hujaifasoft211@gmail.com>
2025-09-02 12:05:04 +0530

    Microservices

--- aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
+
 public class GeminiService {
 
     private final WebClient webClient;
@@ -21,23 +23,22 @@ public class GeminiService {
         this.webClient = webClientBuilder.build();
     }
 
-    public String getAnswer(String question) {
-        Map<String, Object> requestBody = Map.of(
-                "contents", new Object[] {
-                        Map.of("parts", new Object[]{
-                                Map.of("text", question)
+    public String getRecommendation(String details) {
+        Map<String, Object>  requestBody = Map.of(
+                "contents", new Object[]{
+                        Map.of("parts",new Object[]{
+                                Map.of("text", details)
                         })
                 }
         );
-
         String response = webClient.post()
-                .uri(geminiApiUrl + geminiApiKey)
-                .header("Content-Type", "application/json")
+                .uri(geminiApiUrl)
+                .header("Content-Type","application/json")
+                .header("X-good-api-key", geminiApiKey)
                 .bodyValue(requestBody)
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
-
         return response;
     }
 }
